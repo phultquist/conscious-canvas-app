f50cd2c05d9950f6486be60c1640f07acd0ba465
phultquist <patrick@playgroundai.com>
2023-07-30 22:36:11 -0700

    make microphone work

--- Conscious Canvas/ContentView.swift
+++ Conscious Canvas/ContentView.swift
@@ -1,21 +1,40 @@
-//
-//  ContentView.swift
-//  Conscious Canvas
-//
-//  Created by Patrick Hultquist on 7/30/23.
-//
-
 import SwiftUI
+import WebKit
+import AVFoundation
+
+func requestMicrophoneAccess() {
+    AVAudioSession.sharedInstance().requestRecordPermission { granted in
+        if granted {
+            // The user granted access to the microphone
+        } else {
+            // The user did not grant access to the microphone
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            WebView(url: URL(string: "http://localhost:8080"))
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    requestMicrophoneAccess()
+                }
         }
-        .padding()
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL?
+
+    func makeUIView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        if let url = url {
+            webView.load(URLRequest(url: url))
+        }
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // Update the view if needed
     }
 }
 
