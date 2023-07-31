fbc59e2c7cbbce95e7f05b42731580670345cec7
phultquist <patrick@playgroundai.com>
2023-07-30 21:52:00 -0700

    Initial Commit

---
+++ Conscious Canvas/Conscious_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conscious_CanvasApp.swift
+//  Conscious Canvas
+//
+//  Created by Patrick Hultquist on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Conscious_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conscious CanvasUITests/Conscious_CanvasUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Conscious_CanvasUITestsLaunchTests.swift
+//  Conscious CanvasUITests
+//
+//  Created by Patrick Hultquist on 7/30/23.
+//
+
+import XCTest
+
+final class Conscious_CanvasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
